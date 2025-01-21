a553ba06ea71f28dde74734ed918e12a8a9bf2c8
dungad2k2 <dung9d32k2@gmail.com>
2025-01-21 14:52:55 +0700

    update show hidden files

--- init.lua
+++ init.lua
@@ -4,4 +4,5 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 require("config.lazy")
 require("config.keymap")
+vim.o.clipboard = "unnamedplus"
 vim.cmd([[colorscheme tokyonight-moon]])
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,5 +6,17 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function ()
+      require('neo-tree').setup {
+        filesystem = {
+          filtered_items = {
+            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+            hide_dotfiles = false,
+            hide_gitignored = true,
+	  }
+	}
     }
+    end 
+      
 }
